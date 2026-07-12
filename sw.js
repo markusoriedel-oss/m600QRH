be3aedd1b9a6d7ae258147be9e1d810d2aa6da4c
markusoriedel-oss <markus.o.riedel@googlemail.com>
2026-07-12 15:25:50 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'm600qrh-v1.3';
+const CACHE = 'm600qrh-app-0.2';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
